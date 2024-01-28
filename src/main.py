bdc978f1cc5b538634b83333580fbf9f90716f98
Alex Eastlake <alex.eastlake@gmail.com>
2024-01-28 17:59:23 +1300

    Refactoring for better separation of components

--- src/main.py
+++ src/main.py
@@ -4,21 +4,62 @@ from utils import pdf_utils
 
 def main():
     try:
-        # Searches for a wikipedia article using user input
-        article_title = wikipedia_api_utils.search_article_titles()
+        # Searches for a wikipedia page using user input
+        page_title = search_page_titles()
 
-        # Retrieves various article info
-        article_url = wikipedia_api_utils.get_article_url(article_title)
-        article_text = wikipedia_api_utils.get_article_text(article_title)
-        article_thumbnail = wikipedia_api_utils.get_article_thumbnail(article_title)
-        article_images = wikipedia_api_utils.get_article_images(article_title, 3)
+        # Retrieves various page data
+        content = {
+            "get": ["text", "thumbnail", "images", "source"],
+            "max_images": 3,
+            "image_file_types": [".jpeg", ".jpg", ".png"]
+        }
 
-        # Exports article data to PDF
+        page_content = get_page_contents(page_title, content)
+
+        # Exports page data to PDF
         output_dir = os.path.join(os.path.normpath(os.path.join(os.path.dirname(__file__), "..")), "output")
-        pdf_utils.export_pdf(dir = output_dir, title = article_title, text = article_text, thumbnail = article_thumbnail, images = article_images, url = article_url)
+        pdf_utils.export_pdf(dir = output_dir, title = page_title, content = page_content)
     except Exception as e:
-        print(e)
+        print("An error occurred: \n{}".format(e))
         exit()
+
+# Loop to search for page titles using user input
+def search_page_titles():
+    try:
+        while True:
+            search_term = input("Enter a search term:")
+
+            page_titles = wikipedia_api_utils.get_page_titles(search_term)
+            
+            if page_titles and len(page_titles) == 1:
+                return page_titles[0]
+            elif page_titles and len(page_titles > 1):
+                print("Search results: {}".format(page_titles.join(", ")))
+            else:
+                print("No results found")
+    except Exception as e:
+        raise e
+
+def get_page_contents(page_title, content):
+    try:
+        data = {}
+
+        for element in content["get"]:
+            match element:
+                case "text":
+                    data["text"] = wikipedia_api_utils.get_page_text(page_title)
+                case "thumbnail":
+                    data["thumbnail"] = wikipedia_api_utils.get_page_thumbnail(page_title)
+                case "images":
+                    if data.keys().contains("max_images"):
+                        data["images"] = wikipedia_api_utils.get_page_images(page_title, content["max_images"])
+                        data["images"] = wikipedia_api_utils.get_page_images(page_title, content["file_types"], content["max_images"])
+                case "source":
+                    data["source"] = wikipedia_api_utils.get_page_url(page_title)   
+
+        return data
+    except Exception as e:
+        raise e
 
 if __name__ == "__main__":
     main()
--- src/utils/wikipedia_api_utils.py
+++ src/utils/wikipedia_api_utils.py
@@ -3,75 +3,44 @@ import random
 
 API_URL = "https://en.wikipedia.org/w/api.php"
 
-# Gets titles of articles as a search result for the given article title
-def get_article_titles(article_title):
+# Gets the content of a URL
+def get_url_content(url):
     try:
-        if not article_title:
-            raise
+        return requests.get(url = url, headers = {"User-Agent": "Script"}, allow_redirects = True).content
+    except Exception as e:
+        raise e
 
-        print("Getting articles for {}...".format(article_title))
-
+def search_page_titles(page_title):
+    try:
         request_params = {
             "action": "query",
             "list": "search",
-            "srsearch": article_title,
+            "srsearch": page_title,
             "srlimit": 10,
             "redirects": True,
             "format": "json"
         }
 
-        response = requests.get(API_URL, request_params)
-        response_json = response.json()
+        response_json = (requests.get(API_URL, request_params)).json()
 
         pages = (response_json.get("query").get("search"))
 
-        if len(pages) <= 0:
-            raise
+        titles = []
 
-        titles = [page.get("title") for page in pages]
-
-        print("Retrieved articles {}\n".format(", ".join(titles)))
+        if pages:
+            for page in pages:
+                titles.append(page.get("title"))
 
         return titles
     except Exception as e:
-        print("Failed to retrieve articles: {\n}".format(e))
+        raise e
 
-        return []
-
-# Loop to search for article titles using user input
-def search_article_titles():
+# Gets page intro text from page title
+def get_page_text(page_title):
     try:
-        while True:
-            search_title = input("Enter a title, or press Enter to exit: ")
-
-            if not search_title:
-                exit()
-
-            article_titles = get_article_titles(search_title)
-            
-            if len(article_titles) == 1:
-                return article_titles[0]
-            elif len(article_titles) <= 0:             
-                continue
-            elif len(article_titles) > 1:
-                for i, title in enumerate(article_titles):
-                    if search_title.lower() == title.lower():
-                         if input("A retrieved article has title: {}. Confirm this article (y/n)? ".format(article_titles[i])) == "y":
-                            return article_titles[i]
-               
-                print("More than 1 article found, narrow your title search\n")
-                continue
-    except Exception as e:
-        print("Failed to search articles: {}".format(e))
-
-# Gets article intro text from article title
-def get_article_text(article_title):
-    try:
-        print("Getting article text...")
-
         request_params = {
             "action": "query",
-            "titles": article_title,
+            "titles": page_title,
             "prop": "extracts",
             "exintro": True,
             "explaintext": True,
@@ -80,28 +49,17 @@ def get_article_text(article_title):
             "format": "json"
         }
 
-        response = requests.get(API_URL, request_params)
-        response_json = response.json()
+        response_json = (requests.get(API_URL, request_params)).json()
 
         page_text = next(iter((response_json.get("query").get("pages").values()))).get("extract")
-        print("Retrieved article text\n")
 
         return page_text
-    except Exception as e:
-        print("Failed to retrieve article text: {}\n".format(e))
-
-# Gets the content of a URL
-def get_url_content(url):
-    try:
-        return requests.get(url = url, headers = {"User-Agent": "Script"}, allow_redirects = True).content
     except Exception as e:
         raise e
 
 # Gets an image from wikipedia with the given image title
-def get_article_image(image_title):
+def get_image(image_title):
     try:
-        print("Getting article image {}...".format(image_title))
-    
         request_params = {
             "action": "query",
             "titles": image_title,
@@ -110,68 +68,89 @@ def get_article_image(image_title):
             "format": "json"
         }
 
-        response = requests.get(API_URL, request_params)
-        response_json = response.json()
+        response_json = (requests.get(API_URL, request_params)).json()
 
         image_url = next(iter((response_json.get("query").get("pages").values()))).get("imageinfo")[0].get("url")
         image = get_url_content(image_url)
-        print("Retrieved article image\n")
 
         return image
     except Exception as e:
-        print("Failed to retrieve article image: {}\n".format(e))
+        raise e
 
-# Gets the given max number of random images from the article with the given title
-def get_article_images(article_title, num_images):
+def get_page_image_titles(page_title):
     try:
-        print("Getting {} article {} image(s)...".format(num_images, article_title))
-
         request_params = {
             "action": "query",
-            "titles": article_title,
+            "titles": page_title,
             "prop": "images",
             "imlimit": "max",
             "redirects": True,
             "format": "json"
         }
 
-        response = requests.get(API_URL, request_params)
-        response_json = response.json()
-
-        valid_file_types = [".jpg", ".jpeg", ".png"]
+        response_json = (requests.get(API_URL, request_params)).json()
 
         page = next(iter((response_json.get("query").get("pages").values())))
 
         if "images" not in page.keys():
-            raise ValueError("No images found for page")
+            raise ValueError("No page images found for page {}".format(page_title))
 
-        page_images = page.get("images")
-        page_image_titles = [image.get("title") for image in page_images]
-        filtered_page_image_titles = [title for title in page_image_titles if any(title.lower().endswith(file_type) for file_type in valid_file_types)]
-        chosen_indexes = random.sample(range(0, len(filtered_page_image_titles)), min(len(filtered_page_image_titles), num_images))
-        chosen_page_image_titles = [filtered_page_image_titles[i] for i in chosen_indexes]
+        image_titles = [image.get("title") for image in page.get("images")]
+
+        return image_titles
+    except Exception as e:
+        raise e
+
+def filter_image_titles_filetype(image_titles, file_types):
+    try:
+        valid_image_titles = []
+
+        for title in image_titles:
+            if any(title.lower().endswith(file_type.lower()) for file_type in file_types):
+                valid_image_titles.append(title)
+            
+        if not valid_image_titles:
+            raise ValueError("No page images found in valid formats {}".format(file_types.join(", ")))
+    except Exception as e:
+        raise e
+
+def get_random_image_titles(image_titles, max_images):
+    try:
+        chosen_titles = []
+
+        for i in random.sample(range(len(image_titles)), min(len(image_titles), max_images)):
+            chosen_titles.append(image_titles[i])
+    except Exception as e:
+        raise e
+
+# Gets the given max number of random images from the page with the given title
+def get_page_images(page_title, file_types, max_images):
+    try:
+        image_titles = get_page_image_titles(page_title)
+
+        if file_types:
+            image_titles = filter_image_titles_filetype(image_titles, file_types)
         
+        if max_images:
+            image_titles = get_random_image_titles(image_titles, max_images)
+
         images = []
 
-        for image_title in chosen_page_image_titles:
-            if len(images) >= num_images: break
-
-            images.append(get_article_image(image_title))
-
-        print("Retrieved {} article image(s)\n".format(len(images)))
+        for image_title in image_titles:
+            images.append(get_image(image_title))
         
         return images
     except Exception as e:
-        print("Failed to retrieve article image(s): {}\n".format(e))
+        raise e
 
-# Gets the thumbnail image for an article with the given title
-def get_article_thumbnail(article_title):
+# Gets the thumbnail image for a page with the given title
+def get_page_thumbnail(page_title):
     try:
-        print("Getting article {} thumbnail...".format(article_title))
+        print("Getting page {} thumbnail...".format(page_title))
 
         request_params = {
             "action": "query",
-            "titles": article_title,
+            "titles": page_title,
             "prop": "pageimages",
             "piprop": "thumbnail",
             "pithumbsize": 200,
@@ -179,8 +158,7 @@ def get_article_thumbnail(article_title):
             "format": "json"
         }
 
-        response = requests.get(API_URL, request_params)
-        response_json = response.json()
+        response_json = (requests.get(API_URL, request_params)).json()
 
         page = next(iter((response_json.get("query").get("pages").values())))
 
@@ -191,20 +169,20 @@ def get_article_thumbnail(article_title):
         thumbnail_url = thumbnail.get("source")
 
         thumbnail_image = get_url_content(thumbnail_url)
-        print("Retrieved article thumbnail\n")
+        print("Retrieved page thumbnail\n")
 
         return thumbnail_image
     except Exception as e:
-        print("Failed to retrieve article thumbnail: {}\n".format(e))
+        print("Failed to retrieve page thumbnail: {}\n".format(e))
 
-# Gets the URL of an article with the given title
-def get_article_url(article_title):
+# Gets the URL of a page with the given title
+def get_page_url(page_title):
     try:
-        print("Getting article {} URL...".format(article_title))
+        print("Getting page {} URL...".format(page_title))
 
         request_params = {
             "action": "query",
-            "titles": article_title,
+            "titles": page_title,
             "prop": "info",
             "inprop": "url",
             "redirects": True,
@@ -215,8 +193,8 @@ def get_article_url(article_title):
         response_json = response.json()
 
         url = next(iter((response_json.get("query").get("pages").values()))).get("fullurl")
-        print("Retrieved article URL\n")
+        print("Retrieved page URL\n")
 
         return url
     except Exception as e:
-        print("Failed to retrieve article {} URL".format(article_title))
+        print("Failed to retrieve page {} URL".format(page_title))
